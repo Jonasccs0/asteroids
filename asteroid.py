866e4a1bc10f7514eb99f74218e8844caf8d62ae
jonasccs0 <jonasccsantana@gmail.com>
2024-10-09 10:29:56 -0300

    Added shooting, colission and destruction

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,6 @@
 from circleshape import *
 from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +11,19 @@ class Asteroid(CircleShape):
     
     def update(self,dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        else:
+            angle = random.uniform(20,50)
+            vec1 = self.velocity.rotate(angle)
+            vec2 = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            ast1 = Asteroid(self.position.x, self.position.y, new_radius)
+            ast2 = Asteroid(self.position.x, self.position.y, new_radius)
+            ast1.velocity = vec1*1.2
+            ast2.velocity = vec2*1.2
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 
 def main():
     print("Starting asteroids!")
@@ -14,10 +15,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids =  pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
 
     pygame.init()
     clock = pygame.time.Clock()
@@ -40,6 +43,11 @@ def main():
             if ast.collision(player) == True:
                 print('Game over!')
                 return
+            
+            for bullet in shots:
+                if ast.collision(bullet) == True:
+                    ast.split()
+                    bullet.kill()
 
         for dr in drawable:
             dr.draw(screen)
