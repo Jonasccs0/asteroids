6a38d15526ca01f21f500dbf4673f3ffc95ab793
jonasccs0 <jonasccsantana@gmail.com>
2024-10-09 09:53:46 -0300

    Added asteroids, collision and movement

--- main.py
+++ main.py
@@ -3,17 +3,50 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids =  pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    afield = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("#000000")
+            
+        screen.fill("black")
+
+        for up in updatable:
+            up.update(dt)
+
+        for ast in asteroids:
+            if ast.collision(player) == True:
+                print('Game over!')
+                return
+
+        for dr in drawable:
+            dr.draw(screen)
+
         pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
+
 if __name__ == "__main__":
     main()
